6a27090f35e422e1cbaea169dd94fe00edb622eb
crazypig <362370932@qq.com>
2024-03-28 16:32:19 +0800

    init

--- analyze/R/溯源分析FEAST.R
+++
@@ -1,6 +0,0 @@
-Sys.setlocale(category = 'LC_ALL', locale = 'English_United States.1252')
-library(FEAST)
-metadata <- Load_metadata(metadata_path = "./metadata_fungi_t.txt")
-otus <- Load_CountMatrix(CountMatrix_path = "./out_fungi_t.txt")
-
-FEAST_output <- FEAST(C = otus, metadata = metadata, different_sources_flag = 0, dir_path = "./", outfile = "fungi_tradictional")
